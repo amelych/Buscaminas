e4f3f5ecb24df26b6c77b91ab7b06f8b22e0f843
AmelyECH <116146886+amelych@users.noreply.github.com>
2023-04-12 09:17:02 +0200

    juego mejorado + recursividad

--- src/buscaminas/Buscaminas.java
+++ src/buscaminas/Buscaminas.java
@@ -13,7 +13,8 @@ public class Buscaminas {
      */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in, "ISO-8859-1");
-        Xogo x = new Xogo(6, 6, 6);
+        Xogo x = new Xogo(6, 6, 4);
+        Vista pruebas = new Vista();
         boolean continuar = x.isVivo();
         int eleccion = 0;
         int fila = 0;
@@ -22,12 +23,12 @@ public class Buscaminas {
         do {
             System.out.println("-----------------Menu-----------------");
             System.out.println("1. Escolle a cela a destapar (fila e columna)");
-            System.out.println("2. Marcar unha cela (marcar a cela cun 2)");
+            System.out.println("2. Marcar unha cela.");
             System.out.println("3. salir");
             x.imprimirPanel();
             eleccion = teclado.nextInt();
             switch (eleccion) {
-                case 1:
+                case 1 -> {
                     System.out.println("Escribe a fila: ");
                     fila = teclado.nextInt();
                     System.out.println("Escribe a columna: ");
@@ -36,21 +37,22 @@ public class Buscaminas {
                     if (x.getCela(fila, columna).isMinada()) {
                         continuar = x.isVivo();
                     } else if (x.comprobarCelasAbrir() == true) {
-                        System.out.println("Juego ganado!!!!");
+                        x.imprimirPanel();
+                        x.juegoGanado();
                         continuar = false;
                     }
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Escribe a fila: ");
                     fila = teclado.nextInt();
                     System.out.println("Escribe a columna: ");
                     columna = teclado.nextInt();
                     x.marcarCela(x.getCela(fila, columna));
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     System.out.println("Saíndo.....");
                     continuar = false;
-                    break;
+                }
             }
         } while (continuar);
     }
--- src/buscaminas/Xogo.java
+++ src/buscaminas/Xogo.java
@@ -16,12 +16,14 @@ public class Xogo {
     private Cela[][] celas;
     private boolean vivo;
     private ArrayList<Cela> celasAdxacentes;
+    private Vista vista;
 
     public Xogo(int filas, int columnas, int minas) {
         this.filas = filas;
         this.columnas = columnas;
         this.celas = new Cela[filas][columnas];
         this.vivo = true;
+        vista = new Vista();
         for (int i = 0; i < celas.length; i++) {
             for (int j = 0; j < celas[i].length; j++) {
                 celas[i][j] = new Cela(i, j, false);
@@ -71,14 +73,18 @@ public class Xogo {
                     if (celas[i][j].isMinada() == true) {
                         System.out.print("[" + "*" + "]");
                     } else {
-                        System.out.print("[" + " " + "]");
+                        axudaUsuario(celas[i][j]);
                     }
                 }
             }
             System.out.println();
         }
-
         for (int i = 0; i < celas.length; i++) {
+            System.out.print("\t" + i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < celas.length; i++) {
+            System.out.print(i + "\t");
             for (int j = 0; j < celas[i].length; j++) {
                 System.out.print("[" + celas[i][j].isMinada() + "]");
             }
@@ -86,56 +92,59 @@ public class Xogo {
         }
     }
 
+    private void axudaUsuario(Cela cela) {
+        switch (getMinasAdxacentes(cela)) {
+            case 1 ->
+                System.out.print("[" + vista.CYAN + "1" + vista.RESET + "]");
+            case 2 ->
+                System.out.print("[" + vista.GREEN + "2" + vista.RESET + "]");
+            case 3 ->
+                System.out.print("[" + vista.RED + "3" + vista.RESET + "]");
+            case 4 ->
+                System.out.print("[" + vista.PURPLE + "4" + vista.RESET + "]");
+            case 5 ->
+                System.out.println("[" + vista.YELLOW + "5" + vista.RESET + "]");
+            case 6 ->
+                System.out.println("[" + vista.CYAN + "6" + vista.RESET + "]");
+            case 7 ->
+                System.out.println("[" + vista.BLACK + "7" + vista.RESET + "]");
+            case 8 ->
+                System.out.println("[" + vista.BLACK_ON_RED + "8" + vista.RESET + "]");
+            default ->
+                System.out.print("[" + " " + "]");
+        }
+    }
+
     public Cela getCela(int fila, int columna) {
         return celas[fila][columna];
     }
 
-    /* funciona sin bordes
     private ArrayList<Cela> getCelasAdxacentes(Cela cela) {
         celasAdxacentes = new ArrayList<>();
         int fila = cela.getFila();
         int columna = cela.getColumna();
         for (int i = 0; i < celas.length; i++) {
             for (int j = 0; j < celas[i].length; j++) {
-                if (celas[i][j] == celas[fila][columna - 1]) {
-                   celasAdxacentes.add(celas[i][j]);
-                } else if (celas[i][j] == celas[fila][columna + 1]) {
-                    celasAdxacentes.add(celas[i][j]);
-                } else if (celas[i][j] == celas[fila - 1][columna]) {
-                    celasAdxacentes.add(celas[i][j]);
-                } else if (celas[i][j] == celas[fila + 1][columna]) {
-                    celasAdxacentes.add(celas[i][j]);
-                } else if (celas[i][j] == celas[fila - 1][columna - 1]) {
-                    celasAdxacentes.add(celas[i][j]);
-                } else if (celas[i][j] == celas[fila - 1][columna + 1]) {
-                    celasAdxacentes.add(celas[i][j]);
-                } else if (celas[i][j] == celas[fila + 1][columna - 1]) {
-                    celasAdxacentes.add(celas[i][j]);
-                } else if (celas[i][j] == celas[fila + 1][columna + 1]) {
-                    celasAdxacentes.add(celas[i][j]);
-                } 
-            }
-        }
-        return celasAdxacentes;
-    }*/
-    private ArrayList<Cela> getCelasAdxacentes(Cela cela) {
-        celasAdxacentes = new ArrayList<>();
-        int fila = cela.getFila();
-        int columna = cela.getColumna();
-        for (int i = 0; i < celas.length; i++) {
-            for (int j = 0; j < celas[i].length; j++) {
-                if (fila == 0 || fila == celas.length || columna == 0 || columna == celas[i].length) {
-                    if (fila == 0) {
+                if (fila == 0 || fila == celas.length - 1 || columna == 0 || columna == celas[i].length - 1) {
+                    if (fila == 0 && columna == 0) {
                         celasAdxacentes.add(celas[fila + 1][columna]);
                         celasAdxacentes.add(celas[fila + 1][columna + 1]);
                         celasAdxacentes.add(celas[fila][columna + 1]);
-                    } else if (fila == celas.length) {
-                        celasAdxacentes.add(celas[fila - 1][columna]);
-                        celasAdxacentes.add(celas[fila - 1][columna + 1]);
-                        celasAdxacentes.add(celas[fila][columna + 1]);
-                    } else if (columna == 0) {
-                        
+                    } else if (fila == 0 && columna == celas[i].length - 1) {
+                        celasAdxacentes.add(celas[fila][columna - 1]);
+                        celasAdxacentes.add(celas[fila + 1][columna - 1]);
+                        celasAdxacentes.add(celas[fila + 1][columna]);
                     }
+                    if (fila == celas.length - 1 && columna == 0) {
+                        celasAdxacentes.add(celas[fila][columna + 1]);
+                        celasAdxacentes.add(celas[fila - 1][columna + 1]);
+                        celasAdxacentes.add(celas[fila - 1][columna]);
+                    } else if (fila == celas.length - 1 && columna == celas[i].length - 1) {
+                        celasAdxacentes.add(celas[fila][columna - 1]);
+                        celasAdxacentes.add(celas[fila - 1][columna - 1]);
+                        celasAdxacentes.add(celas[fila - 1][columna]);
+                    }
+
                 } else {
                     if (celas[i][j] == celas[fila][columna - 1]) {
                         celasAdxacentes.add(celas[i][j]);
@@ -173,20 +182,24 @@ public class Xogo {
     public void abrirCela(Cela cela) {
         if (celas[cela.getFila()][cela.getColumna()].isMinada() == true) {
             setVivo(false);
-            System.err.println("Abriste una mina");
+            vista.partidaPerdida();
             abrirTodasMinas();
             imprimirPanel();
         } else {
             cela.setEstado(3);
             if (getMinasAdxacentes(cela) == 0) {
-               
+                for (int i = 0; i < getCelasAdxacentes(cela).size(); i++) {
+                    if (getCelasAdxacentes(cela).get(i).getEstado() == 1) {
+                        abrirCela(getCelasAdxacentes(cela).get(i));
+                    }
+                }
             }
         }
     }
 
     public void marcarCela(Cela cela) {
         if (celas[cela.getFila()][cela.getColumna()].getEstado() == 3) {
-            System.err.println("A cela xa estaba destapada");
+            vista.celaDestapada();
         } else {
             cela.setEstado(2);
         }
@@ -226,5 +239,9 @@ public class Xogo {
                 contador++;
             }
         }
+    }
+
+    public void juegoGanado() {
+        vista.partidaGanada();
     }
 }
